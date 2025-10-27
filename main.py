0fc42ba3a1fa796267829e7f30f3efe0abdb7670
Thibault GAREL <thibault.garel@edu.ece.fr>
2025-10-27 12:50:06 +0100

    Update

--- main.py
+++ main.py
@@ -36,7 +36,10 @@ def main():
 
     # Éléments qui se répètent avec espacement ALÉATOIRE:
     parallax.add_layer("img/mountain2.png", depth=0.1, x_position=0, y_position=0, repeat=True, repeat_spacing=(9, 12))
-    parallax.add_layer("img/tree3.png", depth=0.7, x_position=0, y_position=0, repeat=True, repeat_spacing=(8, 15))
+    parallax.add_layer("img/tree3.png", depth=0.7, x_position=0, y_position=0, repeat=True, repeat_spacing=(4, 10))
+    parallax.add_layer("img/tree3.png", depth=0.7, x_position=3, y_position=0, repeat=True, repeat_spacing=(4, 10))
+    parallax.add_layer("img/tree4.png", depth=0.6, x_position=-2, y_position=0, repeat=True, repeat_spacing=(4, 10))
+    parallax.add_layer("img/tree4.png", depth=0.5, x_position=-6, y_position=0, repeat=True, repeat_spacing=(4, 10))
     # parallax.add_layer("cloud.png", depth=0.1, x_position=0, y_position=8, repeat=True, repeat_spacing=(10, 20))
 
     # Éléments qui se répètent avec espacement FIXE:
