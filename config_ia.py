10dc5bbd9ce4e7977f2d2edaa386ed45bdb5d549
Thibault GAREL <thibault.garel@edu.ece.fr>
2025-11-11 15:14:59 +0100

    Update

--- config_ia.py
+++ config_ia.py
@@ -4,10 +4,10 @@
 # ============ OPTIONS PRINCIPALES ============
 
 # Mode de contrôle
-HUMAN_CONTROL = False  # True = contrôle humain, False = contrôle IA
+HUMAN_CONTROL = True  # True = contrôle humain, False = contrôle IA
 
 # Affichage
-DISPLAY_ENABLED = False  # True = afficher l'écran, False = mode rapide sans affichage
+DISPLAY_ENABLED = True  # True = afficher l'écran, False = mode rapide sans affichage
 
 # ============ PARAMÈTRES DE L'ALGORITHME GÉNÉTIQUE ============
 
